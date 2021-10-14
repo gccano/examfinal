5986d9d13841905edc75e5bd64b7c8ce382683d1
gccano <91982368+gccano@users.noreply.github.com>
2021-10-14 14:44:52 -0600

    Add files via upload

---
+++ js/inicio.js
@@ -0,0 +1,9 @@
+function go(){
+    if (document.formingreso.usuario.value=='juana' && document.formingreso.contraseña.value=='examen')
+    {document.formingreso.submit();}
+
+    else{
+        alert("Ingrese su usuario y contraseña correctos.");
+    }
+}
+
